d683c1305d404242b7b954a6c7766f9ae93ea688
Eoghan Bradshaw <eoghan.bradshaw@gmail.com>
2023-06-02 16:37:37 +0400

    Update Face_Detector.py

--- Face_Detector.py
+++ Face_Detector.py
@@ -4,8 +4,6 @@ from random import randrange
 trained_face_data = cv2.CascadeClassifier(
     'haarcascade_frontalface_default.xml')
 
-# read image
-# img = cv2.imread('wingTeam.jpg')
 webcam = cv2.VideoCapture(0)
 
 
